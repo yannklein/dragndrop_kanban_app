ae7b39dea7b237326a5ca069c4cf5752d33a2f81
Yann Klein <yann.klein@me.com>
2020-09-30 16:26:23 +0900

    added Card and KanbanColumn destroy_all in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Card.destroy_all
+KanbanColumn.destroy_all
 Kanban.destroy_all
 
 my_kanban = Kanban.create(
